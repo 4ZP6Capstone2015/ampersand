985fa8c227b7b7d0c68fd7c85f63d0ef003ce446
yuriy0 <toporoy@mcmaster.ca>
2016-01-06 13:52:22 -0500

    Add printInfo function

--- src/Main.hs
+++ src/Main.hs
@@ -24,9 +24,14 @@ main =
               Errors err    -> do putStrLn "Error(s) found:"
                                   mapM_ putStrLn (intersperse  (replicate 30 '=') (map showErr err))
                                   exitWith $ ExitFailure 10
-              Checked fSpec -> do when printName $ putStrLn $ "The name of the specification is " ++ fsName fSpec 
+              Checked fSpec -> do when printName $ printInfoFSpec fSpec 
                                   generateAmpersandOutput fSpec
                                   generateProtoStuff      fSpec
+
+printInfoFSpec :: FSpec -> IO ()  
+printInfoFSPec fSpec = do
+  putStrLn $ "The name of the specification is " ++ fsName fSpec 
+  
 
 generateProtoStuff :: FSpec -> IO ()
 generateProtoStuff fSpec
