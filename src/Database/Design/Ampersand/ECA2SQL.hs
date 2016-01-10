0356587d74aa419f8584fad9ef4a37f3db7ffc57
yuriy0 <toporoy@mcmaster.ca>
2016-01-10 17:37:38 -0500

    Add ECA2SQL module

---
+++ src/Database/Design/Ampersand/ECA2SQL.hs
@@ -0,0 +1,19 @@
+module Database.Design.Ampersand.ECA2SQL where 
+
+import Database.Design.Ampersand.Core.AbstractSyntaxTree
+import Database.Design.Ampersand.FSpec 
+import Language.SQL.SimpleSQL.Syntax
+
+
+
+data SQLStatement 
+  = Insert TableRef QueryExpr  
+  | Delete TableRef QueryExpr 
+  | Update TableRef [(Name, QueryExpr)] 
+  | Block [SQLStatement] 
+
+
+eca2SQL :: FSpec -> ECArule -> SQLStatement 
+eca2SQL fSpec (ECA trigger delta action _) = _ 
+
+
